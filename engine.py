f4c2eb1817cc434b8f5c333d8eb70b8c9aac2302
Irvan <dolphinigle.dropbox@gmail.com>
2016-03-30 14:37:57 +0800

    classifier.

--- engine.py
+++ engine.py
@@ -3,6 +3,8 @@ Created on Mar 30, 2016
 
 @author: dolphinigle
 '''
+from collections import namedtuple
+
 import models
 
 
@@ -15,4 +17,47 @@ def TestCombineResults(normal_reads, tumor_reads, in_snvs):
                            anomaly.pos,
                            anomaly.pos))
   return snvs
+
+
+TestData = namedtuple('TestData', ['normal_read',
+                                   'tumor_read',
+                                   'vcfs',
+                                   'bed'])
+# Additionally, it may have .bed: truth value from the input and .actual: truth calculated by classifier.
+
+
+class Learner(object):
+  def __init__(self, test_datas):
+    # learn datas is a list of TestData
+    pass
+
+  def Classify(self, test_data):
+    # Return result for this test data.
+    pass
+
+
+class NaiveLearner(object):
+  def __init__(self, test_datas):
+    # learn datas is a list of TestData
+    pass
+
+  def Classify(self, test_data):
+    vcfs = test_data.vcfs[0]
+    snvs = []
+    for anomaly in vcfs:
+      snvs.append(models.SNV(anomaly.chrom,
+                             anomaly.pos,
+                             anomaly.pos))
+    return snvs
+
+
+class DecisionTreeLearner(object):
+  def __init__(self, test_datas):
+    # learn datas is a list of TestData
+    pass
+
+  def Classify(self, test_data):
+    # fill in the "bed" value in given test_data.
+    pass
+
 
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Created on Mar 30, 2016
 import os.path
 import parser
 
+from engine import TestData
 import engine
 import eval_lib
 
@@ -22,25 +23,53 @@ NORMAL_INPUT_FILENAME = '{0}_normal.bam'
 TUMOR_INPUT_FILENAME = '{0}_tumor.bam'
 TESTDATA_FOLDER = 'testdata'
 
-def Run(method, folder_name):
-  tumor_input = parser.ReadBAM(os.path.join(
-      TESTDATA_FOLDER, folder_name, NORMAL_INPUT_FILENAME.format(folder_name)))
-  normal_input = parser.ReadBAM(os.path.join(
-      TESTDATA_FOLDER, folder_name, TUMOR_INPUT_FILENAME.format(folder_name)))
-  truth = parser.ReadBED(os.path.join(
-      TESTDATA_FOLDER, folder_name, TRUTH_FILENAME.format(folder_name)))
-  results = [parser.ReadVCF(os.path.join(TESTDATA_FOLDER, folder_name, vcf_filename.format(folder_name))) for vcf_filename in RESULT_FILENAMES]
-  print 'Running {0} on {1}...'.format(method.func_name, folder_name)
-  run_result = method(normal_input, tumor_input, results)
-  print 'Run completed.'
-  f1, precision, recall = eval_lib.Evaluate(truth, run_result)
-  print 'F1 score: {0}'.format(f1)
-  print 'Precision: {0}'.format(precision)
-  print 'Recall: {0}'.format(recall)
-  return f1, precision, recall
+def Run(classifier_class, sample_folders, test_folders):
+  print 'Preprocessing sample datas {0}...'.format(sample_folders)
+  sample_datas = []
+  for folder_name in sample_folders:
+    normal_input = parser.ReadBAM(os.path.join(
+        TESTDATA_FOLDER, folder_name, TUMOR_INPUT_FILENAME.format(folder_name)))
+    tumor_input = parser.ReadBAM(os.path.join(
+        TESTDATA_FOLDER, folder_name, NORMAL_INPUT_FILENAME.format(folder_name)))
+    results = [parser.ReadVCF(os.path.join(TESTDATA_FOLDER, folder_name, vcf_filename.format(folder_name))) for vcf_filename in RESULT_FILENAMES]
+    truth = parser.ReadBED(os.path.join(
+        TESTDATA_FOLDER, folder_name, TRUTH_FILENAME.format(folder_name)))
+    sample_datas.append(TestData(normal_input,
+                                 tumor_input,
+                                 results,
+                                 truth))
+
+  print 'Preprocessing test datas {0}...'.format(test_folders)
+  test_datas = []
+  for folder_name in test_folders:
+    normal_input = parser.ReadBAM(os.path.join(
+        TESTDATA_FOLDER, folder_name, TUMOR_INPUT_FILENAME.format(folder_name)))
+    tumor_input = parser.ReadBAM(os.path.join(
+        TESTDATA_FOLDER, folder_name, NORMAL_INPUT_FILENAME.format(folder_name)))
+    results = [parser.ReadVCF(os.path.join(TESTDATA_FOLDER, folder_name, vcf_filename.format(folder_name))) for vcf_filename in RESULT_FILENAMES]
+    #truth = None
+    truth = parser.ReadBED(os.path.join(
+        TESTDATA_FOLDER, folder_name, TRUTH_FILENAME.format(folder_name)))
+    test_datas.append(TestData(normal_input,
+                               tumor_input,
+                               results,
+                               truth))
+
+  print 'Building {0} from {1}...'.format(classifier_class.__name__, sample_folders)
+  classifier = classifier_class(test_datas)
+  print 'Classifier constructed.'
+
+  for test_data, test_folder in zip(test_datas, test_folders):
+    print 'Running classifier on {0}'.format(test_folder)
+    result = classifier.Classify(test_data)
+    f1, precision, recall = eval_lib.Evaluate(test_data.bed, result)
+    print 'F1 score: {0}'.format(f1)
+    print 'Precision: {0}'.format(precision)
+    print 'Recall: {0}'.format(recall)
+
 
 if __name__ == '__main__':
-  Run(engine.TestCombineResults, 'syn1')
+  Run(engine.NaiveLearner, ['syn1'], ['syn1'])
 
 
 
